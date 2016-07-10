68b10d395b70a6c8e7a8daf5bddc490cd36f537b
Jennifer Davis <sparklydevops@gmail.com>
2016-07-10 02:21:07 +0000

    updating recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,25 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+baseurl = "http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/"
+
+if node["kernel"]["machine"] != "x86_64"
+  baseurl = "http://downloads-distro.mongodb.org/repo/redhat/os/i686/"
+end
+
+template "/etc/yum.repos.d/mongodb.repo"  do
+  variables(
+    :baseurl => baseurl
+  )
+end
+
+package "mongodb-org" do
+  action :install
+end
+
+# Start MongoDB.
+# # ensure that MongoDB will start following a system reboot
+service "mongod" do
+  action [:start, :enable]
+end
